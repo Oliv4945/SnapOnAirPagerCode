f46996639847302ef9ea49b732225626bbd448aa
Dr CADIC Philippe <pcadic@gmail.com>
2020-03-24 21:30:18 +0100

    Add files via upload

--- RFM_LORA_RX.ino
+++ RFM_LORA_RX.ino
@@ -23,18 +23,41 @@
 #define rst 0
 #define dio0 22
 
+// Buzzer configuration
+int freq = 2000;
+int channel = 0;
+int resolution = 8;
+
 String LoRaData;
+String Received="Not received";
+int updatedisplay=0;
+
+// Sound on off function
+void rotarybeep(int channel, int freq, int dura){
+  // makes a small noise (if no MUTE on) freq in hz and duration in msec
+  ledcWriteTone(channel, freq);
+  delay(dura);
+  ledcWriteTone(channel, 0);
+  };
+
+  
 
 void setup() {
 
+   // Buzzer config
+   ledcSetup(channel, freq, resolution);
+   ledcAttachPin(32, channel);
+
   // DISPLAY
-    uc1701Init(DC_PIN, RST_PIN, LED_PIN, CS_PIN, MOSI_PIN, SCK_PIN, FLIP180, INVERT, -1);
+    uc1701Init(DC_PIN, RST_PIN, LED_PIN, CS_PIN, MOSI_PIN, SCK_PIN, FLIP180, INVERT, -1);    
     uc1701SetContrast(235);
     uc1701Fill(0);    
     uc1701WriteString(0,1,(char *)"LORA SETUP OK", 0, FONT_NORMAL);
     uc1701WriteString(0,2,(char *)"Listening ...", 0, FONT_NORMAL);
-
-
+    uc1701WriteString(0,3,(char *)"Waiting lora data", 0, FONT_NORMAL);
+    
+   
+    
     //initialize Serial Monitor
   Serial.begin(115200);
   while (!Serial);
@@ -56,6 +79,9 @@ void setup() {
   // ranges from 0-0xFF
   LoRa.setSyncWord(0xF3);
   Serial.println("LoRa Initializing OK!");
+
+ 
+   
 }
 
 void loop() {
@@ -69,20 +95,34 @@ void loop() {
     while (LoRa.available()) {
       LoRaData = LoRa.readString();
       Serial.print(LoRaData); 
+      Received="Got a datagram";
+      updatedisplay=1;
+     
     }
 
     // print RSSI of packet
     Serial.print("' with RSSI ");
     Serial.println(LoRa.packetRssi());
     String LevelS="RSSI"+LoRa.packetRssi();
-
-    // DISPLAY
-    uc1701Init(DC_PIN, RST_PIN, LED_PIN, CS_PIN, MOSI_PIN, SCK_PIN, FLIP180, INVERT, -1);
-    uc1701SetContrast(235);
-    uc1701Fill(0);    
-    uc1701WriteString(0,1,(char *)"Got it", 0, FONT_NORMAL);
-    uc1701WriteString(0,2,(char *)"Level", 0, FONT_NORMAL);
-
-    
+     Serial.println(Received); 
+          rotarybeep(0, 250, 50);
+ 
   }
+
+   
+   if (updatedisplay==1){ // if display needs to be changed
+    delay(100);
+    Serial.println("Trying init od Display");
+    uc1701Init(DC_PIN, RST_PIN, LED_PIN, CS_PIN, 23, SCK_PIN, FLIP180, INVERT, -1);    
+    Serial.println("Trying to clear display");
+    uc1701Fill(0);    
+    Serial.println("Printing the result");
+    uc1701WriteString(0,1,(char *)"Packet received", FONT_SMALL,0);
+    uc1701WriteString(0,2,(char *)"Go one packet", FONT_SMALL,0);
+    uc1701WriteString(0,3,(char *)"Waiting next...", FONT_SMALL,0);
+
+    updatedisplay=0;
+    };
+    
+  
 }
